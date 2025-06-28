2d95badfc0a0a4b8064dc685d1c008c7791eabc9
aanthonymax <anthonymaxgit@gmail.com>
2025-06-28 16:08:23 +0300

    add init fn

---
+++ dist/index.d.ts
@@ -0,0 +1,60 @@
+import { compile, stringify } from "../build/main";
+
+import {
+  HMPLRequestInit,
+  HMPLInstanceContext,
+  HMPLRequestContext,
+  HMPLRequestInitFunction,
+  HMPLInstance,
+  HMPLRequest,
+  HMPLRequestGet,
+  HMPLHeadersInit,
+  HMPLIdentificationRequestInit,
+  HMPLCompile,
+  HMPLTemplateFunction,
+  HMPLRequestInfo,
+  HMPLIndicator,
+  HMPLContentTypes,
+  HMPLIndicatorTrigger,
+  HMPLRequestStatus,
+  HMPLCompileOptions,
+  HMPLAutoBodyOptions,
+  HMPLDisallowedTag,
+  HMPLDisallowedTags,
+  HMPLSanitize,
+  HMPLClearInterval
+} from "../build/types";
+
+const hmpl = {
+  compile,
+  stringify
+};
+
+export { compile, stringify };
+
+export default hmpl;
+
+export type {
+  HMPLRequestInit,
+  HMPLInstanceContext,
+  HMPLRequestContext,
+  HMPLRequestInitFunction,
+  HMPLInstance,
+  HMPLRequest,
+  HMPLRequestGet,
+  HMPLHeadersInit,
+  HMPLIdentificationRequestInit,
+  HMPLCompile,
+  HMPLTemplateFunction,
+  HMPLRequestInfo,
+  HMPLIndicator,
+  HMPLContentTypes,
+  HMPLIndicatorTrigger,
+  HMPLRequestStatus,
+  HMPLCompileOptions,
+  HMPLAutoBodyOptions,
+  HMPLDisallowedTag,
+  HMPLDisallowedTags,
+  HMPLSanitize,
+  HMPLClearInterval
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import hmpl from "hmpl-js";
-const templates = document.querySelectorAll("template[data-hmpl]");
 
-templates.forEach((template) => {
-  const templateHTML = template.innerHTML;
-  const templateFn = hmpl.compile(templateHTML);
-  const result = templateFn(() => ({})).response;
-  template.replaceWith(result as any);
-});
+const hmplDOM = (() => {
+  let initialized = false;
+  let initParams = () => ({});
+
+  function mountTemplates() {
+    const templates = document.querySelectorAll("template[data-hmpl]");
+
+    templates.forEach((template) => {
+      const templateHTML = template.innerHTML;
+      const templateFn = hmpl.compile(templateHTML);
+      const result = templateFn(initParams).response;
+      template.replaceWith(result);
+    });
+  }
+
+  function init(paramsFn = () => ({})) {
+    if (initialized) return;
+    initialized = true;
+    initParams = paramsFn;
+    mountTemplates();
+  }
+
+  queueMicrotask(() => {
+    if (!initialized) {
+      mountTemplates();
+    }
+  });
+
+  return { init };
+})();
+
+export default hmplDOM;
